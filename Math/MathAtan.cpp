9b7c3cc0c5b017383c704bcbf88ff6cef66bf8b9
banli-liyunjie <66787091+banli-liyunjie@users.noreply.github.com>
2021-06-29 18:17:44 +0800

    Add files via upload

--- Math/MathAtan.cpp
+++ Math/MathAtan.cpp
@@ -5,18 +5,25 @@ namespace math
 {
 	double atan(double num)
 	{
-		double numt = num > 1 ? 1 / num : num;//arctan(x)+arctan(1/x)=Pi/2
-		if (numt >= 0.999999)//arctan(x)x->1 收敛慢
-			return 0.785398;
+		double numt = num > 1 ? 1 / num //arctan(x)+arctan(1/x)=Pi/2
+			: num < -1 ? -(1/num)
+			: num < 0 ? -num : num;
 		double n = numt, sum = 0;
-		int i = 0;
-		do
+		if (numt >= 0.999999)//arctan(x)x->1 收敛慢
+			sum = 0.785398;
+		else
 		{
-			sum += n;
-			i++;
-			n = -n * numt * numt * (2.0 * i - 1) / (2.0 * i + 1);
-		} while (n < -0.000000001 || n > 0.000000001);
-		sum = num > 1 ? Pi / 2 - sum : sum;
+			int i = 0;
+			do
+			{
+				sum += n;
+				i++;
+				n = -n * numt * numt * (2.0 * i - 1) / (2.0 * i + 1);
+			} while (n < -0.000000001 || n > 0.000000001);
+		}
+		sum = num > 1 ? Pi / 2 - sum
+			: num < -1 ? -(Pi / 2 - sum)
+			: num < 0 ? -sum : sum;
 		sum = 1.0 * (int)(sum * 1000000) / 1000000;
 		return sum;
 	}
