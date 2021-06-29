aeaa31fec95f534f5df6e8cbbfc33cac3f844d0e
RuoChen-Zhang <68012457+RuoChen-Zhang@users.noreply.github.com>
2021-06-29 17:34:22 +0800

    Add files via upload

---
+++ MathAsin.cpp
@@ -0,0 +1,20 @@
+#include "MathASin.h"
+
+namespace math
+{
+	double asin(double num)
+	{
+		if (num < -1 || num > 1)
+			throw num;
+		double n = num, sum = 0;
+		int i = 0;
+		do
+		{
+			sum += n;
+			i++;
+			n = n * num * num * (2.0 * i - 1) * (2.0 * i - 1) / (2.0 * i) / (2.0 * i + 1);
+		} while (n < -0.000000001 || n>0.000000001);
+		sum = 1.0 * (int)(sum * 1000000) / 1000000;
+		return sum;
+	}
+}
